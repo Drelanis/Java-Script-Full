58f6ad611cf5c7e85fea2538af1091c154883df4
Drelanis <denisbadakaty51@gmail.com>
2023-01-05 21:09:23 +0200

    1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 for (let firstNumber = 1; firstNumber <= 5; firstNumber += 1) {
-  console.log(`=> ${firstNumber} <=`);
+  console.log(`
+  => ${firstNumber} <=
+  `);
   for (let secondNumber = 1; secondNumber <= 10; secondNumber += 1) {
     console.log(
       `${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`
