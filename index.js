9712e38272c5d5befb23f893d7cb880cb1c94516
Drelanis <denisbadakaty51@gmail.com>
2023-01-05 20:42:01 +0200

    1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+for (let firstNumber = 1; firstNumber <= 5; firstNumber += 1) {
+  console.log(`=> ${firstNumber} <=`);
+  for (let secondNumber = 1; secondNumber <= 10; secondNumber += 1) {
+    console.log(
+      `${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`
+    );
+  }
+}
