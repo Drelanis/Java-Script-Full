22dc309e8ae24f76f196ac901df96d3196d80e13
Drelanis <denisbadakaty51@gmail.com>
2023-01-05 22:18:56 +0200

    1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const start = 11;
+const end = 23;
+let result = 0;
 
+for (let number = start; number <= end; number += 1) {
+  if (number % 5 === 0) {
+    console.log(number);
+  } else if (number % 2 === 0 && number % 4 !== 0) {
+    result += number;
+  } else if (number % 3 === 0) {
+    result -= number;
+  } else if (number % 4 === 0) {
+    result *= number;
+  }
+}
