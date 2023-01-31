0e777c37f123dc57f33d789f6d3865bb829b3edd
Drelanis <denisbadakaty51@gmail.com>
2023-01-31 17:44:05 +0200

    1

--- index.js
+++ index.js
@@ -17,10 +17,3 @@ const ADULT_AGE = 18;
 
 export const getAdults = obj =>
   Object.fromEntries(Object.entries(obj).filter(element => element[1] >= ADULT_AGE));
-
-const a = { 'John Doe': 19, Tom: 17, Bob: 18 };
-const b = { Ann: 56, Andrey: 7 };
-
-console.log(getAdults(a)); // ==> { 'John Doe': 19, Bob: 18 }
-console.log(getAdults(b)); // ==> { Ann: 56 }
-console.log(getAdults({})); // ==> { Ann: 56 }
