81306cb2d6c773a9217eb3e8692b1341a1693aff
Drelanis <denisbadakaty51@gmail.com>
2023-01-31 14:51:57 +0200

    1

--- calculator.js
+++
@@ -1,5 +0,0 @@
-export const getSquaredArray = array => array.map(element => element * element);
-
-export const getOddNumbers = array => array.filter(element => element % 2 === 1);
-
-export default (firstOperand, secondOperand) => firstOperand + secondOperand;
---
+++ getMinSquaredNumber.js
@@ -0,0 +1,3 @@
+const getMinSquaredNumber = array => Math.min(...array.map(element => element * element));
+
+// console.log(getMinSquaredNumber([-777, 3, -2, 6, 45, -20]));
