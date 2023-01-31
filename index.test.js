56f89c7ff805d1a65bc3f7502b8ceda4d5a4180d
Drelanis <denisbadakaty51@gmail.com>
2023-01-31 14:30:04 +0200

    1

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 import importType from 'eslint-plugin-import/lib/core/importType';
-import { getSquaredArray, getOddNumbers, getSum } from './index.js';
+import { getSquaredArray, getOddNumbers, getSum } from './calculator.js';
 
 it('should get squarred numbers in array', () => {
   const result = getSquaredArray([1, 2, 3, 4]);
