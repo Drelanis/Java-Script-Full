1e0d402385badd225f630e33d0a82e590b638bd6
Drelanis <denisbadakaty51@gmail.com>
2023-01-31 15:59:02 +0200

    1

--- getMinSquaredNumber.js
+++ getMinSquaredNumber.js
@@ -1,1 +1,10 @@
-const getMinSquaredNumber = array => Math.min(...array.map(element => element * element));
+export const getMinSquaredNumber = array => {
+  if (!Array.isArray(array) || array.length <= 0) {
+    return null;
+  }
+  return Math.min(...array.map(element => element * element));
+};
+
+console.log(getMinSquaredNumber([-777, 3, -2, 6, 45, -20])); // => 4
+console.log(getMinSquaredNumber([])); // => null
+console.log(getMinSquaredNumber('[-777, 3, -2, 6, 45, -20]')); // => null
--- getMinSquaredNumber.test.js
+++ getMinSquaredNumber.test.js
@@ -1,6 +1,7 @@
 import { getMinSquaredNumber } from './getMinSquaredNumber.js';
 
 it('should get min squared number', () => {
-  const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
-  expect(result).toEqual(4);
+  expect(getMinSquaredNumber([-777, 3, -2, 6, 45, -20])).toEqual(4);
+  expect(getMinSquaredNumber('[-777, 3, -2, 6, 45, -20]')).toEqual(null);
+  expect(getMinSquaredNumber([])).toEqual(null);
 });
