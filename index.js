0472e10ce6599bd09964fba3eac04c0262c67f9a
Drelanis <denisbadakaty51@gmail.com>
2023-02-13 16:07:50 +0200

    1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const getItemsList = () => {
+export const getItemsList = () => {
   const elementList = document.querySelectorAll('.technology');
   console.dir(elementList);
   return elementList;
 };
 
-const getItemsArray = () => {
+export const getItemsArray = () => {
   const elementsArray = Array.from(document.querySelectorAll('.tool'));
   console.dir(elementsArray);
   return elementsArray;
