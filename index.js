6b1049c2c866e79ebb8263f21342e4886fddf297
Drelanis <denisbadakaty51@gmail.com>
2023-02-13 15:47:59 +0200

    1

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-'use strict';
-
-export const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
-];
-
-const renderTasks = tasksList => {
-  const listElem = document.querySelector('.list');
-
-  const listItemsElements = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      const checkboxElem = document.createElement('input');
-      checkboxElem.setAttribute('type', 'checkbox');
-      checkboxElem.checked = done;
-      checkboxElem.classList.add('list__item-checkbox');
-      listItemElem.append(checkboxElem, text);
-      return listItemElem;
-    });
-
-  listElem.append(...listItemsElements);
+const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
 };
 
-renderTasks(tasks);
+const getInputElement = () => {
+  const inputElem = document.querySelector('input[type=text]');
+  console.dir(inputElem);
+};
+
+getTitleElement();
+getInputElement();
