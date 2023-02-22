4a5b13baf155976e5432684f875f3117ad2bccde
Drelanis <denisbadakaty51@gmail.com>
2023-02-22 11:58:00 +0200

    1

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-const arenaElement = document.querySelector('.arena');
-const boardELement = document.querySelector('.board__selected-seat');
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
 
-const createArrayofNumberRange = (from, to) => {
-  const array = [];
+const listElem = document.querySelector('.list');
+const buttonCreatTaskElement = document.querySelector('.create-task-btn');
+const inputTaskElement = document.querySelector('.task-input');
+let IDNumber = 0;
 
-  for (let index = from; index <= to; index += 1) {
-    array.push(index);
-  }
-  return array;
+const renderTasks = tasksList => {
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.classList.add('list__item-checkbox');
+      checkbox.dataset.id = IDNumber;
+      checkbox.checked = done;
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      checkbox.dataset.id = IDNumber;
+      IDNumber += 1;
+      listItemElem.append(checkbox, text);
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
 };
+renderTasks(tasks);
 
-const createArenaSeats = () => {
-  return createArrayofNumberRange(1, 10)
-    .map(seatNumber => `<div class="sector__seat" data-seat-number = "${seatNumber}"></div>`)
-    .join('');
-};
-
-const createArenaLines = () => {
-  return createArrayofNumberRange(1, 10)
-    .map(
-      linesNumber =>
-        `<div class="sector__line" data-line-number = "${linesNumber}">${createArenaSeats()}</div>`,
-    )
-    .join('');
-};
-
-const createArenaSectors = () => {
-  arenaElement.innerHTML = createArrayofNumberRange(1, 3)
-    .map(
-      sectorNumber =>
-        `<div class="sector" data-sector-number = "${sectorNumber}">${createArenaLines()}</div>`,
-    )
-    .join('');
-};
-
-createArenaSectors();
-
-const getSeatSelect = event => {
-  const isSeat = event.target.classList.contains('sector__seat');
-  if (!isSeat) {
+const tasksToDo = event => {
+  if (event.target.dataset.id === undefined) {
     return;
   }
-  const getSeatNumber = event.target.dataset.seatNumber;
-  const getLineNumber = event.target.closest('.sector__line').dataset.lineNumber;
-  const getSectorNumber = event.target.closest('.sector').dataset.sectorNumber;
-
-  boardELement.innerHTML = `S ${getSectorNumber} - L ${getLineNumber} - S ${getSeatNumber}`;
+  tasks[Number(event.target.dataset.id)].done = event.target.checked;
+  listElem.innerHTML = '';
+  renderTasks(tasks);
 };
 
-arenaElement.addEventListener('click', getSeatSelect);
+const addNewTasks = () => {
+  if (inputTaskElement.value === '') {
+    return;
+  }
+  tasks.push({ text: `${inputTaskElement.value}`, done: false });
+  listElem.innerHTML = '';
+  renderTasks(tasks);
+};
+
+listElem.addEventListener('click', tasksToDo);
+buttonCreatTaskElement.addEventListener('click', addNewTasks);
